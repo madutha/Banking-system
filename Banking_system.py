de142f46ce32ae3d147e8faf9e573b698ed18a0a
ganeshmadutha <161416621+madutha@users.noreply.github.com>
2025-03-06 15:25:58 +0530

    Update Banking_system.py

--- Banking_system.py
+++ Banking_system.py
@@ -2,38 +2,48 @@
 balance = 0
 is_running = True
 
+     
+def saving_account():
+    account_properties()
 
-def show_balance():
-    global balance
-    print(f"your balance is \u20B9{balance:.2f}")
+def current_account():
+    account_properties()
 
-def withdraw():
-    global balance
-    amount = float(input("Enter your withdraw amount:"))
+def account_properties():
 
-    if amount > balance:
-        print("insufficent balance!")
-    elif amount <= 0:
-        print("amount must be greater than zero")
-    else:
-        balance-=amount
-        print(f"withdraw amount \u20B9{amount:.2f}")
+    global is_running
 
-def deposit():
-    global balance
-    amount = float(input("Enter your deposite amount:"))
+    def show_balance():
+        global balance
+        print(f"your balance is \u20B9{balance:.2f}")
 
-    if amount <= 0:
-        print("Thats not a valid amount")
-    else:
-        balance+=amount
-        print(f"Deposited amount \u20B9{amount:.2f}")
+    def withdraw():
+        global balance
+        amount = float(input("Enter your withdraw amount:"))
 
-def Exit():
-    is_running = False
+        if amount > balance:
+            print("insufficent balance!")
+        elif amount <= 0:
+            print("amount must be greater than zero")
+        else:
+            balance-=amount
+            print(f"withdraw amount \u20B9{amount:.2f}")
+
+    def deposit():
+        global balance
+        amount = float(input("Enter your deposite amount:"))
+
+        if amount <= 0:
+            print("Thats not a valid amount")
+        else:
+            balance+=amount
+            print(f"Deposited amount \u20B9{amount:.2f}")
+
+    def exit_program():
+        global is_running
+        is_running = False
 
 
-while is_running:
     print("1.Deposite")
     print("2.show Balance")
     print("3.Withdraw")
@@ -48,7 +58,25 @@ while is_running:
     elif choice == "3":
         withdraw()
     elif choice == "4":
-        exit()
+        exit_program()
     else:
         print("invalid choice!")
 
+while is_running:
+    print("choose your account")
+    print("1.Saving account")
+    print("2.Current account")
+    print("3.Exit")
+
+    account = input("\nEnter your choice:")
+
+    if account == "1":
+        saving_account()
+    elif account == "2":
+        current_account()
+    elif account == "3":
+        is_running = False
+    else:
+        print("Invalid input")
+
+    
